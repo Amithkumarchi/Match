29f1c472e2d80b987a29fb749517048a02885848
Amithkumarchi <amith.kumarchi@gmail.com>
2022-02-27 21:23:45 +0530

    match

--- src/main/java/com/springboot/match/controller/Controller.java
+++ src/main/java/com/springboot/match/controller/Controller.java
@@ -1,11 +1,19 @@
 package com.springboot.match.controller;
 
-import org.springframework.web.bind.annotation.*;
+import com.springboot.match.model.Player;
+import com.springboot.match.repository.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller
 {
 
+    @Autowired
+    PlayerRepository playerRepository;
     @GetMapping(value = "/",produces = "text/html")
     public String healthCheck()
     {
@@ -13,11 +21,17 @@ public class Controller
     }
 
 
-    @GetMapping(value = "/id",produces = "text/html")
-    public String test()
+    @PostMapping(value = "/register", produces = "text/html")
+    public String test(@RequestBody Player player)
 {
+
+   playerRepository.save(player);
+
+
     return "abc";
 }
+
+
 
 
 }
--- src/main/java/com/springboot/match/model/Player.java
+++ src/main/java/com/springboot/match/model/Player.java
@@ -1,18 +1,28 @@
 package com.springboot.match.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-
+@Entity
 public class Player {
 
+@Id
+@GeneratedValue
 int id;
 String name;
+String dateOfBirth;
+String email;
+String status;
+String noShow;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+    public Player(String name, String dateOfBirth, String email, String status, String noShow) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+        this.status = status;
+        this.noShow = noShow;
     }
 
     public String getName() {
@@ -21,5 +31,37 @@ String name;
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getNoShow() {
+        return noShow;
+    }
+
+    public void setNoShow(String noShow) {
+        this.noShow = noShow;
     }
 }
